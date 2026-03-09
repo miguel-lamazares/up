096774473d88c9d317e3deace5a8962699deaa15
Miguel Lamazares <miguellamazares8@gmail.com>
2026-03-09 13:46:42 -0300

    update dashboard

--- backend/app/static/js/dashboard.js
+++ backend/app/static/js/dashboard.js
@@ -42,16 +42,14 @@ logout.addEventListener('click', async (e) => {
 });
 
 
+const menuBtn = document.getElementById("menu-btn");
+const closeBtn = document.getElementById("close-btn");
+const sidebar = document.querySelector("aside");
 
-const sideMenu = document.querySelector("aside");
-const menuBtn = document.querySelector("#menu-btn");
-const closeBtn = document.querySelector("#close-btn");
+menuBtn.onclick = () => {
+    sidebar.classList.add("active");
+};
 
-menuBtn.addEventListener('click', () => {
-    sideMenu.style.display = 'block';
-});
-
-closeBtn.addEventListener('click', () => {
-    sideMenu.style.display = 'none';
-});
-
+closeBtn.onclick = () => {
+    sidebar.classList.remove("active");
+};
